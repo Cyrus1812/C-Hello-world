6dd42575b96f9b1281b44d17533b3097a554d67f
MAX <maksimdemidovru@mail.ru>
2022-08-17 17:31:26 +0300

    add if else

---
+++ Example005_HelloМАША/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите ваше имя ");
+string username = Console.ReadLine();
+
+if(username == "Маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
