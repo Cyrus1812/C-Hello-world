39185a6c8f272e8989310ddb41acdc5d5f46550f
MAX <maksimdemidovru@mail.ru>
2022-08-17 17:34:37 +0300

    add ToLower

--- Example005_HelloМАША/Program.cs
+++ Example005_HelloМАША/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Введите ваше имя ");
 string username = Console.ReadLine();
 
-if(username == "Маша")
+if(username.ToLower() == "маша")
 {
     Console.WriteLine("Ура, это же МАША!");
 }
